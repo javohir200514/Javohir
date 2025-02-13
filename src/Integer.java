b5cffbae58368471991a94a0adf7ac136c66da8e
Javohir <komilbayevj@gmail.com>
2025-02-13 15:03:55 +0500

    IDE files

---
+++ src/Integer.java
@@ -0,0 +1,135 @@
+import java.util.Scanner;
+
+public class Integer {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //1-misol
+        /*System.out.println("Uzunlikni kiriting=");
+        int L=sc.nextInt();
+        int metr=L/100;
+        System.out.println(L+" sm"+"--->"+" "+metr+" metr");*/
+
+        //2-misol
+       /* System.out.println("Og'irlikni kiriting=");
+        int M=sc.nextInt();
+        int tonna=M/1000;
+        System.out.println(M+" kg"+"--->"+" "+tonna+" tonna");*/
+
+        //3-misol
+       /* System.out.println("Fayl hajmini kiriting=");
+        int hajm=sc.nextInt();
+        int kbayt=hajm/1024;
+        System.out.println(hajm+" bayt"+"--->"+" "+kbayt+" kb");*/
+
+        //4-misol
+       /* System.out.println("A kesma uzunligini kiriting=");
+        int A=sc.nextInt();
+        System.out.println("B kesma uzunligini kiriting=");
+        int B=sc.nextInt();
+        System.out.println("A kesmaga B kesmani "+A/B+" marta joylashtirish mumkin");*/
+
+        //5-misol
+        /*System.out.println("A kesma uzunligini kiriting=");
+        int A=sc.nextInt();
+        System.out.println("B kesma uzunligini kiriting=");
+        int B=sc.nextInt();
+        System.out.println("A kesmaga B kesmani "+A/B+" marta joylashtirish mumkin");
+        System.out.println("A kesmaga B kesmaning joylashmay qolgan qismi "+A%B+" qismi joylashmay qoladi");*/
+
+        //6-misol
+       /* System.out.println("2 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("O'nlar xonasi=" + a / 10 % 10);
+        System.out.println("Birlar xonasi="+a%10);
+*/
+
+        //7-misol
+        /*System.out.println("2 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Raqamlari yig'indisi=" + (a / 10 % 10+a%10));*/
+
+        //8-misol
+        /*System.out.println("2 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a%10*10+a/10%10));*/
+
+        //9-10-misol
+       /* System.out.println("Uch xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Yuzlar xonasidagi son="+a/100%10);
+        System.out.println("O'nlar xonasidagi son="+a/10%10);
+        System.out.println("Birlar xonasidagi son="+a%10);*/
+
+        //11-misol
+        /*System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Raqamlari yig'indisi="+(a%10+a/10%10+a/100%10));*/
+
+        //12-misol
+        /*System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a%10*100+a/10%10*10+a/100%10));*/
+
+        //13-misol
+        /*System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a/10%10*100+a%10*10+a/100%10));*/
+
+        //14-misol
+        /*System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a%10*100+a/100%10*10+a/10%10));*/
+
+        //15-misol
+        /*System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a/10%10*100+a/100%10*10+a%10));*/
+
+        //16-misol
+       /* System.out.println("3 xonali son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Swap="+(a/100%10*100+a%10*10+a/10%10));*/
+
+        //17-misol
+        /*System.out.println("999 dan katta son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Yuzliklar xonasidagi son="+a/100%10);*/
+
+        //18-misol
+        /*System.out.println("999 dan katta son kiriting=");
+        int a=sc.nextInt();
+        System.out.println("Yuzliklar xonasidagi son="+a/1000%10);*/
+
+        //19-misol
+        /*System.out.println("Sekundlar sonini kiriting=");
+        int N=sc.nextInt();
+        System.out.println("Kun boshidan o'tgan minutlar soni="+N/60);*/
+
+        //20-misol
+        /*System.out.println("Sekundlar sonini kiriting=");
+        int N=sc.nextInt();
+        System.out.println("Kun boshidan o'tgan soatlar soni="+N/3600);*/
+
+        //21-misol
+        /*System.out.println("Sekundlar sonini kiriting=");
+        int N=sc.nextInt();
+        System.out.println("Kun boshidan boshlab "+N/60+" minut"+" va "+N%60+" sekund o'tdi");*/
+
+        //22-misol
+      /*  System.out.println("Sekundlar sonini kiriting=");
+        int N=sc.nextInt();
+        System.out.println("Kun boshidan boshlab "+N/3600+" soat"+" va "+N%60+" sekund o'tdi");*/
+
+        //23-misol
+        /*System.out.println("Sekundlar sonini kiriting=");
+        int N=sc.nextInt();
+        System.out.println("Kun boshidan boshlab "+N/3600+" soat"+" va "+(N/60)%60+" minut "+N%60+" sekund o'tdi");
+*/
+
+
+
+
+
+    }
+}
